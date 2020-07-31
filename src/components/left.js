ee2ac61d727077e72cd9ba95dff3f9cdfb026cbc
Malaika <malaikafernandes25@gmail.com>
2020-07-31 21:17:48 +0530

    summarty section

---
+++ src/components/left.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Summary from './summary';
+class Left extends Component {
+   
+    render() {
+        return (
+         <div className="left">
+             <Summary />
+         </div>
+            )
+        }
+    }
+    
+export default Left
